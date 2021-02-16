3a25dfb8ac2896bccab2c1ae8ae224136941cbda
Mila <mila.valentine@gmail.com>
2021-02-15 21:57:25 -0300

    Writes and reads categories

---
+++ repositories/categories/readCategories.js
@@ -0,0 +1,130 @@
+const db = require("../../db/connection");
+const errorHelper = require("../../helpers/errorHelper")
+const tables = require("../tables.json")
+
+/**
+ * @get /categories
+ * @returns {Promise}
+ */
+function getCategories() {
+    db(tables.categoriesTable)
+        .then((data) => {
+            let res = data.length === 0 ? `No products found` : data
+            db.destroy();
+            console.log(res)
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @get categories/{category_id}
+ * @param {Number} id
+ * @returns {Promise}
+ */
+function getCategoryById(id) {
+    db(tables.categoriesTable).where("id", id)
+        .then((data) => {
+            let res = data.length === 0 ? `No category with the ID ${id} was found` : data
+            console.log(res);
+            db.destroy();
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @get categories/{category_id}/products
+ * @param {Number} category_id
+ * @returns {Promise}
+ */
+function getCategoryProducts(category_id) {
+    db(tables.PrdInCatTable).where("category_id", category_id)
+        .then((data) => {
+            let res = data.length === 0 ? `No products in the category of ID ${category_id} were found` : data
+            console.log(res);
+            db.destroy();
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @get categories/{category_id}/watchers
+ * @param {Number} category_id
+ * @returns {Promise}
+ */
+function getCategoryWatchers(category_id) {
+    db(tables.watchedTable).where("category_id", category_id)
+        .then((data) => {
+            let res = data.length === 0 ? `No users watching in the category of ID ${category_id} were found` : data
+            console.log(res);
+            db.destroy();
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @get categories/{category_id}/parents
+ * @param {Number} child_id
+ * @returns {Promise}
+ */
+function getCategoryParents(child_id) {
+    db(tables.catParentsTable).where("child_id", child_id)
+        .then((data) => {
+            let res = data.length === 0 ? `No categories parent of the category of ID ${child_id} were found` : data
+            console.log(res);
+            db.destroy();
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @get categories/{category_id}/children
+ * @param {Number} parent_id
+ * @returns {Promise}
+ */
+function getCategoryChildren(parent_id) {
+    db(tables.catParentsTable).where("parent_id", parent_id)
+        .then((data) => {
+            let res = data.length === 0 ? `No categories children of the category of ID ${parent_id} were found` : data
+            console.log(res);
+            db.destroy();
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+module.exports = { getCategories , getCategoryById , getCategoryProducts , getCategoryWatchers , getCategoryParents, getCategoryChildren };
---
+++ repositories/categories/writeCategories.js
@@ -0,0 +1,94 @@
+const db = require("../../db/connection");
+const errorHelper = require("../../helpers/errorHelper")
+const tables = require("../tables.json")
+
+const category = {
+    "name": "Test",
+}
+
+const child = {
+    "child_id": "1",
+    "parent_id": "1"
+}
+
+const parent = {
+    "parent_id": "1",
+    "child_id": "1"
+}
+
+/**
+ * @post /category
+ * @param {Object} data
+ * @returns {Promise}
+ */
+function insertCategory(data) {
+    db(tables.categoriesTable).insert(data)
+        .then((id) => {
+            db(tables.categoriesTable).where("id", id)
+                .then((data) => {
+                    let res = data.length === 0 ? `No data` : data
+                    console.log(res)
+                    db.destroy();
+                    return res;
+                })
+        })
+        .catch((error) => {
+            let err = errorHelper(error);
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @post /category/{category_id}/child/{child_id}
+ * @param {Object} data
+ * @returns {Promise}
+ */
+function insertChild(data) {
+    db(tables.catParentsTable).insert(data)
+        .then((id) => {
+            db(tables.catParentsTable).where("id", id)
+                .then((data) => {
+                    let res = data.length === 0 ? `No data` : data
+                    console.log(res)
+                    db.destroy();
+                    return res;
+                })
+        })
+        .catch((error) => {
+            let err = errorHelper(error);
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @post /category/{category_id}/parent/{parent_id}
+ * @param {Object} data
+ * @returns {Promise}
+ */
+function insertParent(data) {
+    db(tables.catParentsTable).insert(data)
+        .then((id) => {
+            db(tables.catParentsTable).where("id", id)
+                .then((data) => {
+                    let res = data.length === 0 ? `No data` : data
+                    console.log(res)
+                    db.destroy();
+                    return res;
+                })
+        })
+        .catch((error) => {
+            let err = errorHelper(error);
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+//insertCategory(category);
+//insertChild(child);
+//insertParent(parent);
+module.exports = { insertCategory , insertChild , insertParent };
