341af8e7f3730db6561e39672058a01c3e56ce9c
Mila <mila.valentine@gmail.com>
2021-02-15 22:09:25 -0300

    Adds queries to write and read proposals

---
+++ repositories/proposals/readProposals.js
@@ -0,0 +1,71 @@
+const db = require("../../db/connection");
+const errorHelper = require("../../helpers/errorHelper")
+const tables = require("../tables.json")
+
+/**
+ * @get proposals/
+ * @returns {Promise}
+ */
+function getProposals() {
+    db(tables.proposalsTable)
+        .then((data) => {
+            let res = data.length === 0 ? `No proposals found` : data
+            db.destroy();
+            console.log(res)
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @get proposals/{category_id}
+ * @param {Number} id
+ * @returns {Promise}
+ */
+function getProposalsByID(id) {
+    db(tables.proposalsTable).where("id", id)
+        .then((data) => {
+            let res = data.length === 0 ? `No proposal with the ID ${id} was found` : data
+            console.log(res);
+            db.destroy();
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+/**
+ * @get proposals/{proposal_id}/users
+ * @param {Number} proposal_id
+ * @returns {Promise}
+ */
+function getProposalUsers(proposal_id) {
+    db(tables.acceptedTable).where("proposal_id", proposal_id)
+        .then((data) => {
+            let res = data.length === 0 ? `No users who accepted the proposal of ID ${proposal_id} were found` : data
+            console.log(res);
+            db.destroy();
+            return res;
+        })
+        .catch((error) => {
+            let err = errorHelper(error)
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+// getProposals()
+// getProposalsByID(1)
+// getProposalUsers(1);
+
+module.exports = { getProposals , getProposalsByID , getProposalUsers };
---
+++ repositories/proposals/writeProposals.js
@@ -0,0 +1,41 @@
+const db = require("../../db/connection");
+const errorHelper = require("../../helpers/errorHelper")
+const tables = require("../tables.json")
+
+/**
+ * @post proposals/
+ * @param {Object} data
+ * @returns {Promise}
+ */
+function insertProposal(data) {
+    db(tables.proposalsTable).insert(data)
+        .then((id) => {
+            db(tables.proposalsTable).where("id", id)
+                .then((data) => {
+                    let res = data.length === 0 ? `No data` : data
+                    console.log(res)
+                    db.destroy();
+                    return res;
+                })
+        })
+        .catch((error) => {
+            let err = errorHelper(error);
+            console.log(err);
+            db.destroy();
+            return err;
+        })
+}
+
+// const proposal = {
+//     "product_id":"1",
+//     "created_by":"1",
+//     "created_at": new Date(),
+//     "price" : 24.65,
+//     "link":"http://",
+//     "minimun_quantity" : "20",
+//     "requires_intent" : false
+// }
+
+//insertProposal(proposal);
+
+module.exports = { insertProposal };
